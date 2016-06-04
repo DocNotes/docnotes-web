4c8bc2482032244ecaa62756f088589fd977f19a
Julian Koh <juliankoh13@gmail.com>
2016-06-04 13:30:25 +0800

    commit 3

--- ahkl/docnotes/models.py
+++ ahkl/docnotes/models.py
@@ -3,3 +3,17 @@ from __future__ import unicode_literals
 from django.db import models
 
 # Create your models here.
+
+class Patients(models.Model):
+    name = models.CharField(max_length=50, default="Name")
+    photo = models.ImageField(upload_to="patient_photos")
+    UUID = models.CharField(max_length=50, default="UUID")
+    remark = models.CharField(max_length=255, default="Remark")
+
+class Prescription(models.Model):
+	name = models.ForeignKey('Patients', on_delete=models.CASCADE)
+	medication = models.CharField(max_length=255, default="Medication")
+	quantity = models.CharField(max_length=50, default="Quantity")
+
+
+
--- ahkl/docnotes/views.py
+++ ahkl/docnotes/views.py
@@ -1,6 +1,9 @@
 from django.shortcuts import render
 from django.http import HttpResponse, HttpResponseRedirect
+from docnotes.models import Patients, Prescription
+
 
 # Create your views here.
 def index(request):
-	return render(request, "index.html", {})
+    patients = Patients.objects.all()
+    return render(request, "index.html", {'patients' : patients})
